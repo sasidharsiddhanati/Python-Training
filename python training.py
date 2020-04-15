d5d2f80023440dafb8b6566da3a846459c5ccfa6
sasidhar siddhanati <sasidhar.rjy@gmail.com>
2020-04-15 09:59:50 +0530

    Add files via upload

---
+++ python training.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#integer
+x = 2
+
+
+# In[2]:
+
+
+x
+
+
+# In[3]:
+
+
+type(x)
+
+
+# In[4]:
+
+
+#float / double
+x = 2.6
+
+
+# In[5]:
+
+
+x
+
+
+# In[6]:
+
+
+type(x)
+
+
+# In[7]:
+
+
+#string
+a = "hello"
+b = "hello2"
+
+
+# In[8]:
+
+
+a
+
+
+# In[9]:
+
+
+b
+
+
+# In[10]:
+
+
+type(a,b)
+
+
+# In[11]:
+
+
+type(a)
+
+
+# In[12]:
+
+
+type(b)
+
+
+# In[13]:
+
+
+#logical or Bool
+x = True
+y = False
+
+
+# In[14]:
+
+
+x
+
+
+# In[15]:
+
+
+y
+
+
+# In[16]:
+
+
+type(x)
+
+
+# In[17]:
+
+
+type(y)
+
+
+# In[ ]:
+
+
+
+
